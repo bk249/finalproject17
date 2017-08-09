bb99bad00a6167c9b1b8dd7ec4578c23393671ae
Bee Kim <bk249@njit.edu>
2017-08-08 22:49:43 -0400

    added add list

---
+++ additem.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>Add List</title>
+</head>
+
+<body>
+<?php
+//including the database connection file
+include_once("database.php");
+if(isset($_POST['Submit'])) {	
+	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+
+		
+	// checking empty fields
+	if(empty($name)) {
+				
+		if(empty($name)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+	
+		
+		//link to the previous page
+		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+	} else { 
+		// if all the fields are filled (not empty) 
+			
+		//insert data to database	
+		$result = mysqli_query($mysqli, "INSERT INTO lists(name) VALUES('$name')");
+		
+		//display success message
+		echo "<font color='green'>Data added successfully.";
+		echo "<br/><a href='index.php'>View Result</a>";
+	}
+}
+?>
+</body>
+</html>
