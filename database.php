a363835b77498601c2e1d65043fabbad40e47576
Bee Kim <bk249@njit.edu>
2017-08-08 22:52:06 -0400

    database connect

---
+++ database.php
@@ -0,0 +1,9 @@
+
+<?php
+
+$databaseHost = 'sql2.njit.edu';
+$databaseName = 'bk249';
+$databaseUsername = 'bk249';
+$databasePassword = 'rlaqldlqslek91';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
